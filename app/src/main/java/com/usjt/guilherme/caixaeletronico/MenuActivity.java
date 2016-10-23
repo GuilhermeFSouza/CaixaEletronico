501a17700d0be51a7f5601b9b4cf40cb0d432a82
guilherme <gui.souza31@gmail.com>
2016-10-23 19:05:03 -0200

    Upadated references (login)

---
+++ app/src/main/java/com/usjt/guilherme/caixaeletronico/MenuActivity.java
@@ -0,0 +1,47 @@
+package com.usjt.guilherme.caixaeletronico;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import org.json.JSONObject;
+
+/**
+ * Created by guilherme on 23/10/16.
+ */
+public class MenuActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu);
+        
+        Button buttonSaldo = (Button) findViewById(R.id.buttonSaldo);
+        buttonSaldo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, SaldoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button buttonExtrato = (Button) findViewById(R.id.buttonExtrato);
+        buttonExtrato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, ExtratoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button buttonSaque = (Button) findViewById(R.id.buttonSaque);
+        buttonSaque.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(MenuActivity.this, SaqueActivity.class);
+                //startActivity(intent);
+            }
+        });
+    }
+}
